695178be4fff988eb0c09909c6c1d57e923742fe
Ajamu Akil Page <kil91402@gmail.com>
2023-07-11 16:17:20 +0000

    arrow functions complete

--- starter-code/app.js
+++ starter-code/app.js
@@ -116,7 +116,7 @@ let sum = (a, b, c, d) => {
 // console.log(sum(1, 2, 3, 4));
 
 
-let objectLit = function() {
+let objectLit = value =>  {
   return {
     key1: 'value1',
     key2: 'value2',
@@ -128,7 +128,7 @@ let objectLit = function() {
 // console.log(objectLit());
 
 
-let sumAndProduct = function(a, b) {
+let sumAndProduct = (a, b) => {
   let sum = a + b;
   let product = a * b;
   return [sum, product];
@@ -138,7 +138,7 @@ let sumAndProduct = function(a, b) {
 // console.log(sumAndProduct(3, 9));
 
 
-let message = function(name) {
+let message = name => {
   return `Hello, ${name}!`;
 };
 
@@ -146,7 +146,7 @@ let message = function(name) {
 // console.log(message('Allie'));
 
 
-let Student = function(name, age, hometown) {
+let Student = (name, age, hometown) => {
   this.name = name;
   this.age = age;
   this.hometown = hometown;
@@ -162,7 +162,7 @@ let joe = new Student('Joe Schmoe', 100, 'Anytown, USA');
 
 
 
-Student.prototype.greeting = function() {
+Student.prototype.greeting = name => {
   return `Hi, my name is ${this.name}`;
 };
 
@@ -174,7 +174,7 @@ Student.prototype.greeting = function() {
 
 
 
-Student.courseName = function() {
+Student.courseName = course => {
   return 'This student is enrolled in Code 301.';
 };
 
@@ -185,7 +185,7 @@ Student.courseName = function() {
 
 // STEP 11
 // How do arrow functions affect constructor functions?
-Student.prototype.scope = function() {
+Student.prototype.scope = student => {
   console.log(this);
 };
 
